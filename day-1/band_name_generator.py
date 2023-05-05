770a4cfd8cf589c6715d34017a56bf105b7623f8
Neeraj <neerajvarshney142001@gmail.com>
2023-05-05 22:39:41 +0530

    Day-1 work completed

---
+++ day-1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator? \n")
+city_name = input("Which city did you grow up in? \n")
+pet_name = input("what's your pet name \n")
+print(f"your band name suggested by me is {city_name} {pet_name}")
